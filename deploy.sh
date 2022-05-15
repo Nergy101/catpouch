5e6483a8bd56dfa8d7d2f78ce8074392f3b6258f
Christian <chrisdvd@live.nl>
2022-05-15 18:09:34 +0200

    added vite config

--- deploy.sh
+++ deploy.sh
@@ -14,9 +14,6 @@ git checkout -b master
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
-
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
 git push -f git@github.com:Nergy101/catpouch.git master:gh-pages
 
---
+++ vite.config.js
@@ -0,0 +1,9 @@
+/**
+ * @type {import('vite').UserConfig}
+ */
+const config = {
+  base: "/catpouch/",
+  mode: "production",
+};
+
+export default config;
